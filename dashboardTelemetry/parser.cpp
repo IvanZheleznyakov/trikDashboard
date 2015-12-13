cc76423bb8f50c4325024e85807ee2a2be75a7ef
IvanZheleznyakov <iz14@yandex.ru>
2015-12-13 22:27:56 +0400

    realized function with datasource in parser; deleted some comment code

--- dashboardTelemetry/parser.cpp
+++ dashboardTelemetry/parser.cpp
@@ -22,89 +22,21 @@ void Parser::parseMessage(QString message)
 
         QString const deviceName = info.at(0);
 
-        if (!map.contains(deviceName))
-        {
-
+        if (!map.contains(deviceName)) {
+            ElementaryDataSource *newDataSource = new ElementaryDataSource();
+            addDataSource(deviceName, newDataSource);
         }
+
         emit messageIsParsed(deviceName, values);
-
-
-/*        if (deviceName == ACCELEROMETER_NAME && panel.accelerometer->active())
-        {
-            foreach (DashboardWidget *widget, panel.accelerometer->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.accelerometer->newData(values);
-//            panel.accelerometer->widget()->updateData(values);
-        } else
-        if (deviceName == GYROSCOPE_NAME && panel.gyroscope->active())
-        {
-            foreach (DashboardWidget *widget, panel.gyroscope->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.gyroscope->newData(values);
-//            panel.gyroscope->widget()->updateData(values);
-        } else
-        if (deviceName == POWER_MOTOR1_NAME && panel.powerMotor1->active())
-        {
-            foreach (DashboardWidget *widget, panel.powerMotor1->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.powerMotor1->newData(values);
-//            panel.powerMotor1->widget()->updateData(values);
-        } else
-        if (deviceName == POWER_MOTOR2_NAME && panel.powerMotor2->active())
-        {
-            foreach (DashboardWidget *widget, panel.powerMotor2->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.powerMotor2->newData(values);
-//            panel.powerMotor2->widget()->updateData(values);
-        } else
-        if (deviceName == POWER_MOTOR3_NAME && panel.powerMotor3->active())
-        {
-            foreach (DashboardWidget *widget, panel.powerMotor3->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.powerMotor3->newData(values);
-//            panel.powerMotor3->widget()->updateData(values);
-        } else
-        if (deviceName == POWER_MOTOR4_NAME && panel.powerMotor4->active())
-        {
-            foreach (DashboardWidget *widget, panel.powerMotor4->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.powerMotor4->newData(values);
-//            panel.powerMotor4->widget()->updateData(values);
-        } else
-        if (deviceName == BATTERY_NAME && panel.battery->active())
-        {
-            foreach (DashboardWidget *widget, panel.battery->widgets()) {
-                widget->updateData(values);
-            }
-
-            emit panel.battery->newData(values);
-   //         panel.battery->widget()->updateData(values);
-        }
-   */ }
+    }
 }
 
 void Parser::sendData(QString deviceName, QVector<float> values)
 {
-    if (map.contains(deviceName)) {
-        map[deviceName]->readData(values);
-    } else {
-
-    }
+    map[deviceName]->readData(values);
 }
 
 void Parser::addDataSource(QString deviceName, IDataSource *newDataSource)
 {
-
+    map.insert(deviceName, newDataSource);
 }
