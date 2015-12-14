b051597c5372bf3f8aeb8d2bd911eba9b87cadfe
IvanZheleznyakov <iz14@yandex.ru>
2015-12-14 20:47:33 +0400

    changed style of widgetbutton

--- dashboardTelemetry/widgetbutton.cpp
+++ dashboardTelemetry/widgetbutton.cpp
@@ -5,6 +5,8 @@ WidgetButton::WidgetButton(QString text, QString mSensorName) :
     mSensorName(mSensorName)
 {
     WidgetButton::setText(text);
+    WidgetButton::setStyleSheet("QPushButton { background-color: rgb(170, 170, 170); border-style: outset; border-width: 0.5px; border-radius: 5px; border-color: beige; padding: 4px;}"
+                                "QPushButton:pressed { background-color: rgb(200, 200, 200); border-style: inset; }");
 }
 
 QString WidgetButton::getSensorName()
